0fe5ec8f3cf231e893ce47073301a5a2c9d81305
Vivek <10Vivekkumaryadav@gmail.com>
2024-10-30 10:52:57 +0530

    sticky data fetch setup

--- src/components/stockTicker.tsx
+++ src/components/stockTicker.tsx
@@ -1,33 +1,53 @@
 // src/components/StockTicker.tsx
 
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 interface Stock {
   name: string;
+  symbol: string;
   price: number;
-  change: number;
   percentChange: number;
 }
 
-const stocksData: Stock[] = [
-  { name: 'BAJFINANCE', price: 6995.80, change: 4.76, percentChange: 4.76 },
-  { name: 'BHARTIARTL', price: 1685.80, change: -0.38, percentChange: -0.38 },
-  { name: 'HDFCBANK', price: 1735.80, change: 1.24, percentChange: 1.24 },
-  { name: 'HINDUNILVR', price: 2659.30, change: -0.84, percentChange: -0.84 },
-  { name: 'INDIGO', price: 4520.00, change: -0.10, percentChange: -0.10 },
-  { name: 'ITC', price: 480.35, change: -0.30, percentChange: -0.30 },
-  { name: 'MARUTI', price: 9200.75, change: 2.10, percentChange: 2.10 },
-  // Add more stocks as needed
-];
-
 export default function StockTicker() {
+  const [stickyStockData, setStickyStockData] = useState<Stock[]>([]);
+
+  useEffect(() => {
+    const fetchStockData = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:4000/api/stock/getStickyStocks"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setStickyStockData(data);
+        console.log("Stocks updated", data);
+      } catch (error) {
+        console.error("Error fetching stock data:", error);
+      }
+    };
+
+    fetchStockData(); // Initial fetch
+    const intervalId = setInterval(fetchStockData, 2550);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="w-full mt-1 bg-gray-800 text-white h-10 flex items-center overflow-hidden ">
       <div className="ticker-wrapper flex animate-marquee whitespace-nowrap">
-        {stocksData.map((stock, index) => (
+        {stickyStockData.map((stock, index) => (
           <div key={index} className="ticker-item flex mx-4">
-            <span className={`text-sm ${stock.change > 0 ? 'text-green-500' : 'text-red-500'}`}>
-              {stock.change > 0 ? '▲' : '▼'} {Math.abs(stock.percentChange).toFixed(2)}%
+            <span
+              className={`text-sm ${
+                stock.percentChange > 0 ? "text-green-500" : "text-red-500"
+              }`}
+            >
+              {stock.percentChange > 0 ? "▲" : "▼"}{" "}
+              {Math.abs(stock.percentChange).toFixed(2)}%
             </span>
             <span className="mx-2 font-bold">{stock.name}</span>
             <span>{stock.price.toFixed(2)}</span>
@@ -36,4 +56,4 @@ export default function StockTicker() {
       </div>
     </div>
   );
-};
+}
